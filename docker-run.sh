7943ab6a44d713138f0487d8aab10ae3d0a532a8
Edward Thomson <ethomson@microsoft.com>
2019-06-26 16:35:55 +0100

    Update linux kubernetes sample
    - Rename Dockerfile to Dockerfile.linux (to accommodate future Windows
      platform)
    - Create the reserved agent with a common name among all build agents,
      so that there's one reserved agent per pool, to avoid reserved agent
      proliferation.

--- docker-run.sh
+++ docker-run.sh
@@ -10,51 +10,40 @@ if [ -z "$AZURE_PIPELINES_URL" -o -z "$AZURE_PIPELINES_POOL" -o -z "$AZURE_PIPEL
 	exit 1
 fi
 
-IMAGE="ethomson/azure-pipelines-agent:latest"
-
-URL="$AZURE_PIPELINES_URL"
-POOL="$AZURE_PIPELINES_POOL"
-AGENT_NAME="${AZURE_PIPELINES_AGENT_NAME:-$(hostname)}"
-PAT="${AZURE_PIPELINES_PAT}"
-
-unset AZURE_PIPELINES_URL
-unset AZURE_PIPELINES_POOL
-unset AZURE_PIPELINES_AGENT_NAME
-unset AZURE_PIPELINES_PAT
+IMAGE="ethomson/azure-pipelines-agent-linux:latest"
 
 export AGENT_ALLOW_RUNASROOT=1
 
 export AGENT_GUID=$(uuidgen)
+export AGENT_NAME="${AZURE_PIPELINES_AGENT_NAME:-"$(hostname)_${AGENT_GUID}"}"
 
 # Register an agent that will remain idle; we always need an agent in the
 # pool and since our container agents create and delete themselves, there's
 # a possibility of the pool existing with no agents in it, and jobs will
 # fail to queue in this case.  This idle agent will prevent that.
 echo ""
-echo ":: Setting up reserved agent..."
+echo ":: Setting up reserved agent (${AZURE_PIPELINES_POOL}_reserved)..."
 cp -R /data/agent /data/reserved_agent
-"/data/reserved_agent/config.sh" --unattended --url "${URL}" --pool "${POOL}" --agent "${AGENT_NAME}_reserved" --auth pat --token "${PAT}" --replace
+"/data/reserved_agent/config.sh" --unattended --url "${AZURE_PIPELINES_URL}" --pool "${AZURE_PIPELINES_POOL}" --agent "${AZURE_PIPELINES_POOL}_reserved" --auth pat --token "${AZURE_PIPELINES_PAT}" --replace
 
 # Set up the actual runner that will do work.
 echo ""
-echo ":: Setting up runner agent..."
+echo ":: Setting up runner agent (${AGENT_NAME})..."
+rm -rf /data/share/agent
 cp -R /data/agent /data/share
-"/data/share/agent/config.sh" --unattended --url "${URL}" --pool "${POOL}" --agent "${AGENT_NAME}_${AGENT_GUID}" --auth pat --token "${PAT}" --replace
+"/data/share/agent/config.sh" --unattended --url "${AZURE_PIPELINES_URL}" --pool "${AZURE_PIPELINES_POOL}" --agent "${AGENT_NAME}" --auth pat --token "${AZURE_PIPELINES_PAT}"
 
 ret=0
 while [ $ret -eq 0 ]; do
     echo ""
     echo ":: Starting agent..."
     docker run -v "/data/share:/data/share:ro" -e "AGENT_ALLOW_RUNASROOT=1" "${IMAGE}" /bin/sh -c "cp -R /data/share/agent / && /agent/run.sh --once" || ret=$? && true
+    echo ":: Agent exited with: ${ret}"
 done
 
 echo ""
 echo ":: Cleaning up runner agent..."
-/data/share/agent/config.sh remove --auth pat --token "${PAT}"
-
-echo ""
-echo ":: Cleaning up reserved agent..."
-/data/reserved_agent/config.sh remove --auth pat --token "${PAT}"
+/data/share/agent/config.sh remove --auth pat --token "${AZURE_PIPELINES_PAT}"
 
 echo ":: Exiting (exit code ${ret})"
 exit $ret
